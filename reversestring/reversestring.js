817674f5735b5dfc8081664a2d40391014f21e60
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-10 21:15:27 -0700

    solution 3 reverse string

--- reversestring/reversestring.js
+++ reversestring/reversestring.js
@@ -7,17 +7,21 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversed = '';
-  
-  for (let character of str) {
-    reversed = character + reversed;
-  }
-
-  return reversed;
+  return str.split('').reduce((reversed, character) => character + reversed, '');
 }
 
 module.exports = reverse;
 
 // function reverse(str) {
 //   return str.split('').reverse().join('')
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
 // }
