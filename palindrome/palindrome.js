b369e3cc8fdb62b3d2435d40dd66c6571589be1c
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-11 12:53:48 -0700

    palindrome alternative solution with .every()

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,5 +1,13 @@
 function palindrome(str) {
-  return str === str.split('').reduce((reversed, character) => character + reversed,'')
+  return str.split('').every((character, index) => character === str[str.length - index - 1])
 }
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   return str === str.split('').reduce((reversed, character) => character + reversed,'')
+// }
+
+// function palindrome(str) {
+//   return str.split('').reverse().every((element, index) => element === str[index])
+// }
