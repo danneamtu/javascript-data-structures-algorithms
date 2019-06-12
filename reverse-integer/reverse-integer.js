3541a4183b48cbd6fb9333f1bce1c731e3f1821a
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-12 13:07:29 -0700

    third solution

--- reverse-integer/reverse-integer.js
+++ reverse-integer/reverse-integer.js
@@ -1,22 +1,35 @@
 function reverseInteger(n) {
   const multiplier = Math.sign(n);
 
-  if (multiplier > 0) {
-    return +n
+  return (
+    +n
       .toString()
       .split('')
       .reverse()
-      .join('');
-  } else {
-    return (
-      +n
-        .toString()
-        .split('')
-        .filter(negativeSign => negativeSign !== '-')
-        .reverse()
-        .join('') * multiplier
-    );
-  }
+      .filter(num => num !== '-')
+      .join('') * multiplier
+  );
 }
 
 module.exports = reverseInteger;
+
+// function reverseInteger(n) {
+//   const multiplier = Math.sign(n);
+
+//   if (multiplier > 0) {
+//     return +n
+//       .toString()
+//       .split('')
+//       .reverse()
+//       .join('');
+//   } else {
+//     return (
+//       +n
+//         .toString()
+//         .split('')
+//         .filter(negativeSign => negativeSign !== '-')
+//         .reverse()
+//         .join('') * multiplier
+//     );
+//   }
+// }
