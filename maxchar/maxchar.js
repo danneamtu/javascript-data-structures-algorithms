62d84e1483e039a80603d60b246c7068173c57b8
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-13 21:44:42 -0700

    first solution fixed

--- maxchar/maxchar.js
+++ maxchar/maxchar.js
@@ -1,8 +1,24 @@
+// character map
 function maxChar(str) {
-  return str.split('').reduce((obj, char) => {
-    obj[char] = ++obj[char] || 1;
+  let max = 0;
+  let maxCharacter = '';
+
+  str.split('').reduce((obj, char) => {
+    if (obj[char]) {
+      ++obj[char];
+    } else {
+      obj[char] = 1;
+    }
+
+    if (obj[char] > max) {
+      max = obj[char];
+      maxCharacter = char;
+    }
+
     return obj;
   }, {});
+
+  return maxCharacter;
 }
 
 module.exports = maxChar;
