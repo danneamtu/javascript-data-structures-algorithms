9177b96e21622169e45204b9937fcaf7c5eb4206
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-13 21:24:19 -0700

    first solution

--- maxchar/maxchar.js
+++ maxchar/maxchar.js
@@ -1,5 +1,8 @@
-function maxChar() {
-  
+function maxChar(str) {
+  return str.split('').reduce((obj, char) => {
+    obj[char] = obj[char] ? ++obj[char] : 1;
+    return obj;
+  }, {});
 }
 
-module.exports = maxChar
+module.exports = maxChar;
