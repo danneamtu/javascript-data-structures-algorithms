17dde04c1a9f4513b9b9810a8ea198571bccd8c2
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-15 18:45:36 -0700

    second solution from theory

--- array-chunking/array-chunking.js
+++ array-chunking/array-chunking.js
@@ -1,22 +1,39 @@
 function chunk(array, size) {
-  let chunks = [];
+  let chunked = [];
 
-  return array.reduce((result, element, index) => {
-    if (chunks.length < size) {
-      chunks.push(element);
-      if (array.length - 1 === index) {
-        result.push(chunks);
-      }
+  for (let element of array) {
+    let lastChunked = chunked[chunked.length - 1];
+
+    if (!lastChunked || lastChunked.length === size) {
+      chunked.push([element]);
     } else {
-      result.push(chunks);
-      chunks = [];
-      chunks.push(element);
-      if (array.length - 1 === index) {
-        result.push(chunks);
-      }
+      lastChunked.push(element);
     }
-    return result;
-  }, []);
+  }
+
+  return chunked;
 }
 
 module.exports = chunk;
+
+// FIRST SOLUTION:
+// function chunk(array, size) {
+//   let chunks = [];
+
+//   return array.reduce((result, element, index) => {
+//     if (chunks.length < size) {
+//       chunks.push(element);
+//       if (array.length - 1 === index) {
+//         result.push(chunks);
+//       }
+//     } else {
+//       result.push(chunks);
+//       chunks = [];
+//       chunks.push(element);
+//       if (array.length - 1 === index) {
+//         result.push(chunks);
+//       }
+//     }
+//     return result;
+//   }, []);
+// }
