16d0bee1faa65207d367d54819124c2b3b144a65
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-16 06:55:54 -0700

    first solution, character map, for in

--- anagrams/anagrams.js
+++ anagrams/anagrams.js
@@ -1,5 +1,36 @@
 function anagrams(stringA, stringB) {
-  
+  let a = stringA.replace(/[^\w]/g, '').toLowerCase();
+  let b = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    const aMap = a.split('').reduce((characterMap, character) => {
+      if (characterMap[character]) {
+        ++characterMap[character];
+      } else {
+        characterMap[character] = 1;
+      }
+      return characterMap;
+    }, {});
+
+    const bMap = b.split('').reduce((characterMap, character) => {
+      if (characterMap[character]) {
+        ++characterMap[character];
+      } else {
+        characterMap[character] = 1;
+      }
+      return characterMap;
+    }, {});
+
+    for (let key in aMap) {
+      if (aMap[key] !== bMap[key]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 module.exports = anagrams;
