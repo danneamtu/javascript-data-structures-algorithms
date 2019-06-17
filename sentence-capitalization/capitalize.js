20efe1a76480e76723c0a7f3b3f915a4436e134a
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-17 12:31:03 -0700

    second solution - for of

--- sentence-capitalization/capitalize.js
+++ sentence-capitalization/capitalize.js
@@ -1,10 +1,21 @@
 function capitalize(str) {
-  return str
-    .split(' ')
-    .map(word => {
-      return word[0].toUpperCase() + word.slice(1);
-    })
-    .join(' ');
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(' ');
 }
 
 module.exports = capitalize;
+
+// FIRST SOLUTION
+// function capitalize(str) {
+//   return str
+//     .split(' ')
+//     .map(word => {
+//       return word[0].toUpperCase() + word.slice(1);
+//     })
+//     .join(' ');
+// }
