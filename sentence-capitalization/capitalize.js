73b8dea6373f6ce970adf0e4c0f8e0180e826c60
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-17 12:22:53 -0700

    first solution

--- sentence-capitalization/capitalize.js
+++ sentence-capitalization/capitalize.js
@@ -1,5 +1,10 @@
 function capitalize(str) {
-  
+  return str
+    .split(' ')
+    .map(word => {
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join(' ');
 }
 
 module.exports = capitalize;
