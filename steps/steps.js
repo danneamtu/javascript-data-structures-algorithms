4531a6f517265d54d5991e75bb0d5a46baac50bf
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-18 13:33:14 -0700

    second solution

--- steps/steps.js
+++ steps/steps.js
@@ -1,23 +1,40 @@
 function steps(n) {
-  let pounds = '';
+  for (let row = 1; row <= n; row++) {
+    let stair = '';
 
-  const stepsArray = Array.from({ length: n }, (_, index) => {
-    pounds = '';
-
-    for (let i = 0; i < index + 1; i++) {
-      pounds += '#';
+    for (let column = 1; column <= n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
     }
 
-    while (pounds.length < n) {
-      pounds += ' ';
-    }
-
-    return pounds;
-  });
-
-  stepsArray.forEach(step => {
-    console.log(step);
-  });
+    console.log(stair);
+  }
 }
 
 module.exports = steps;
+
+// FIRST SOLUTION
+// function steps(n) {
+//   let pounds = '';
+
+//   const stepsArray = Array.from({ length: n }, (_, index) => {
+//     pounds = '';
+
+//     for (let i = 0; i < index + 1; i++) {
+//       pounds += '#';
+//     }
+
+//     while (pounds.length < n) {
+//       pounds += ' ';
+//     }
+
+//     return pounds;
+//   });
+
+//   stepsArray.forEach(step => {
+//     console.log(step);
+//   });
+// }
