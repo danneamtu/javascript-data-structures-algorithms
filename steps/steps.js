25ebe1e20992c47e18b77c62b76800e999c5d2cc
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-18 06:21:00 -0700

    first solution

--- steps/steps.js
+++ steps/steps.js
@@ -1,5 +1,29 @@
 function steps(n) {
+  let pounds = '';
+  let spaces = '';
 
+  const stepsArray = Array.from({ length: n }, (element, index) => {
+    // if (index < n) {
+    //   pounds += '#';
+    //   return pounds;
+    // }
+
+    pounds = '';
+
+    for (let i = 0; i < index + 1; i++) {
+      pounds += '#';
+    }
+
+    while (pounds.length < n) {
+      pounds += ' ';
+    }
+    
+    return pounds;
+  });
+
+  stepsArray.forEach(step => {
+    console.log(step);
+  });
 }
 
-module.exports = steps
+module.exports = steps;
