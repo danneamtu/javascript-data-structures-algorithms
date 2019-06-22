972444cd68e5068f9e67e5c47d448b67c816eb64
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-22 05:51:45 -0700

    second solution

--- vowels/vowels.js
+++ vowels/vowels.js
@@ -1,21 +1,34 @@
 function vowels(string) {
-  return string.split('').reduce((count, character) => {
-    if (
-      character === 'a' ||
-      character === 'e' ||
-      character === 'i' ||
-      character === 'o' ||
-      character === 'u' ||
-      character === 'A' ||
-      character === 'E' ||
-      character === 'I' ||
-      character === 'O' ||
-      character === 'U'
-    ) {
-      count += 1;
+  let count = 0;
+  const vowelChecker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let character of string.toLowerCase()) {
+    if (vowelChecker.includes(character)) {
+      count++;
     }
-    return count;
-  }, 0);
+  }
+
+  return count;
 }
 
 module.exports = vowels;
+// first solution
+// function vowels(string) {
+//   return string.split('').reduce((count, character) => {
+//     if (
+//       character === 'a' ||
+//       character === 'e' ||
+//       character === 'i' ||
+//       character === 'o' ||
+//       character === 'u' ||
+//       character === 'A' ||
+//       character === 'E' ||
+//       character === 'I' ||
+//       character === 'O' ||
+//       character === 'U'
+//     ) {
+//       count += 1;
+//     }
+//     return count;
+//   }, 0);
+// }
