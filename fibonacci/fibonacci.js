65091b748acc992c5396fac71507c308dffd364f
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-25 11:45:18 -0700

    fibonacci for loop solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-function fibonacci() {
+function fibonacci(n) {
+  let result = [0, 1];
 
+  for (let index = 2; index <= n; index++) {
+    let preceding = result[index - 2];
+    let last = result[index - 1];
+    result.push(preceding + last);
+  }
+
+  return result[result.length - 1]
 }
 
-module.exports = fibonacci
+module.exports = fibonacci;
