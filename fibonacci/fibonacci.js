723a95b33239562f108c6bd3cfe0720ecf1bc8b6
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-25 12:18:08 -0700

    fibonacci recursive solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,13 +1,22 @@
 function fibonacci(n) {
-  let result = [0, 1];
-
-  for (let index = 2; index <= n; index++) {
-    let preceding = result[index - 2];
-    let last = result[index - 1];
-    result.push(preceding + last);
+  if (n < 2) {
+    return n;
   }
 
-  return result[result.length - 1]
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = fibonacci;
+
+// ITERATIVE SOLUTION - LINEAR RUNTIME COMPLEXITY
+// function fibonacci(n) {
+//   let result = [0, 1];
+
+//   for (let index = 2; index <= n; index++) {
+//     let preceding = result[index - 2];
+//     let last = result[index - 1];
+//     result.push(preceding + last);
+//   }
+
+//   return result[result.length - 1]
+// }
