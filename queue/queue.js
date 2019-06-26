acd25889dca3c0d089dc58d5c27403127c264fc7
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-26 09:50:44 -0700

    queue implementation

--- queue/queue.js
+++ queue/queue.js
@@ -3,7 +3,17 @@
 // adding should store element until it is removed
 
 class Queue {
+  constructor() {
+    this.data = [];
+  }
 
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
 }
 
-module.exports = Queue
+module.exports = Queue;
