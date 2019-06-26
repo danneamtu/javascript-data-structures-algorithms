855385a82ec6dc99e9e22316159c8941bc5001e4
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-26 09:39:06 -0700

    queue tests

---
+++ queue/queue.spec.js
@@ -0,0 +1,20 @@
+const Queue = require('./queue')
+
+test('Queue is a class', () => {
+  expect(typeof Queue.prototype.constructor).toEqual('function')
+})
+
+test('can add elements to a queue', () => {
+  const q = new Queue()
+  expect(() => {
+    q.add(1)
+  }).not.toThrow()
+})
+
+test('can remove elements from a queue', () => {
+  const q = new Queue()
+  expect(() => {
+    q.add(1)
+    q.remove()
+  }).not.toThrow()
+})
