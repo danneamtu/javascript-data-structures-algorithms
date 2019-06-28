09cc78dea45a8e2a3eec1d48f7f6f25e3a9a1a48
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-27 20:04:20 -0700

    psudocode weave 2

--- weave/weave.js
+++ weave/weave.js
@@ -1,10 +1,21 @@
-const Queue = require('./queue')
+const Queue = require('./queue');
 
 function weave(sourceOne, sourceTwo) {
-  const weaved = new Queue
+  const weaved = new Queue();
   // while sourceOne.remove() !== undefined
   // weave.add(sourceOne.peek())
 
+  // create a copy of sourceone and source two
+
+  while (false) {
+    if (sourceOne.peek() !== undefined) {
+      weaved.add(sourceOne.peek());
+    }
+
+    if (sourceTwo.peek() !== undefined) {
+      weaved.add(sourceTwo.peek());
+    }
+  }
 }
 
-module.exports = weave
+module.exports = weave;
