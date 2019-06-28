b4e2d969d0bf63a81368ca50cfc8dfeb62ffe464
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-27 20:06:38 -0700

    first weave solution try

--- weave/weave.js
+++ weave/weave.js
@@ -7,7 +7,7 @@ function weave(sourceOne, sourceTwo) {
 
   // create a copy of sourceone and source two
 
-  while (false) {
+  while (sourceOne.peek() !== undefined && sourceTwo.peek() !== undefined) {
     if (sourceOne.peek() !== undefined) {
       weaved.add(sourceOne.peek());
     }
@@ -15,7 +15,12 @@ function weave(sourceOne, sourceTwo) {
     if (sourceTwo.peek() !== undefined) {
       weaved.add(sourceTwo.peek());
     }
+
+    sourceOne.remove();
+    sourceTwo.remove();
   }
+
+  return weaved;
 }
 
 module.exports = weave;
