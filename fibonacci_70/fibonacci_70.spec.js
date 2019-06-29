c2000bbf99795f124b3505c8608c3b80396b2e37
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-28 19:20:54 -0700

    additional test cases

--- fibonacci_70/fibonacci_70.spec.js
+++ fibonacci_70/fibonacci_70.spec.js
@@ -2,6 +2,14 @@ const fibonacci_70 = require('./fibonacci_70');
 
 // [0,1,1,2,3,5,8,13]
 
+test('input value of 0', () => {
+  expect(fibonacci_70(0)).toEqual(0);
+});
+
+test('input value of 1', () => {
+  expect(fibonacci_70(1)).toEqual(1);
+});
+
 test('input value of 5', () => {
   expect(fibonacci_70(5)).toEqual(5);
 });
