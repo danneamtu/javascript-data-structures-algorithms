d887bca43f8846dda6bc03ae76c6efca5faa90e2
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-30 10:47:33 -0700

    stack implementation first

--- queue_from_stacks/queue_from_stacks.js
+++ queue_from_stacks/queue_from_stacks.js
@@ -1,11 +1,25 @@
 // Implement a Queue data structure using 2 stacks.
 
 class Stack {
+  constructor() {
+    this.data = [];
+  }
 
+  push(record) {
+    this.data.push(record);
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
 }
 
 class Queue {
   
 }
 
-module.exports = Queue
+module.exports = Queue;
