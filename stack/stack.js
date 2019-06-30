e167f64474d070a49962cfaf78ee1d13aeb1339e
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-30 09:53:52 -0700

    stack setup

--- stack/stack.js
+++ stack/stack.js
@@ -1,36 +1,5 @@
-const Stack = require('stack');
+class Stack {
 
-test('Stack is a class', () => {
-  expect(typeof Stack.prototype.constructor).toEqual('function');
-});
+}
 
-test('stack can add and remove items', () => {
-  const s = new Stack();
-  s.push(1);
-  expect(s.pop()).toEqual(1);
-  s.push(2);
-  expect(s.pop()).toEqual(2);
-});
-
-test('stack follows first in, last out approach', () => {
-  const s = new Stack();
-  s.push(1);
-  s.push(2);
-  s.push(3);
-  expect(s.pop()).toEqual(3);
-  expect(s.pop()).toEqual(2);
-  expect(s.pop()).toEqual(1);
-});
-
-test('peek returns the top element but does not pop it', () => {
-  const s = new Stack();
-  s.push(1);
-  s.push(2);
-  s.push(3);
-  expect(s.peek()).toEqual(3);
-  expect(s.pop()).toEqual(3);
-  expect(s.peek()).toEqual(2);
-  expect(s.pop()).toEqual(2);
-  expect(s.peek()).toEqual(1);
-  expect(s.pop()).toEqual(1);
-});
+module.exports = Stack
