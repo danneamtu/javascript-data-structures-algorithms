80d7960a08e51b389134e493fde4183ced2765c4
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-01 22:23:19 -0700

    Node test data and next

--- linked_list/linked_list.spec.js
+++ linked_list/linked_list.spec.js
@@ -1,12 +1,19 @@
-const L = require('./linked_list')
-const List = L.LinkedList
-const Node = L.Node
+const L = require('./linked_list');
+const List = L.LinkedList;
+const Node = L.Node;
 
 test('List is a class', () => {
-  expect(typeof List.prototype.constructor).toEqual('function')
-})
+  expect(typeof List.prototype.constructor).toEqual('function');
+});
 
 test('Node is a class', () => {
-  expect(typeof Node.prototype.constructor).toEqual('function')
-})
+  expect(typeof Node.prototype.constructor).toEqual('function');
+});
 
+describe.skip('A Node', () => {
+  test('has properties data and next', () => {
+    const node = new Node('a', 'b');
+    expect(node.data).toEqual('a');
+    expect(node.next).toEqual('b');
+  });
+});
