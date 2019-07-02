edec329918089bfd75787b2a1ba74d407cd2f67d
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-01 22:21:42 -0700

    node is a class test

--- linked_list/linked_list.spec.js
+++ linked_list/linked_list.spec.js
@@ -6,3 +6,7 @@ test('List is a class', () => {
   expect(typeof List.prototype.constructor).toEqual('function')
 })
 
+test('Node is a class', () => {
+  expect(typeof Node.prototype.constructor).toEqual('function')
+})
+
