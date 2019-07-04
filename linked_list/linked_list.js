69b7a45d18c2290a721f8daedbea9eec8d368129
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-03 17:39:13 -0700

    linkedlist first solution linkedlist correct

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -9,6 +9,12 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insertFirst(record) {
+    let currentHeadNode = new Node(this.head);
+    let newHeadNode = new Node(record, currentHeadNode);
+    this.head = newHeadNode;
+  }
 }
 
 module.exports = {Node, LinkedList};
