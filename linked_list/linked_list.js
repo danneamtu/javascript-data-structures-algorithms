07e271c4317a92ea4fa3fe217abab515b579db4c
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-04 22:51:15 -0700

    size - first solution attempt

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -13,6 +13,22 @@ class LinkedList {
   insertFirst(record) {
     this.head = new Node(record, this.head);
   }
+
+  size() {
+    let node = this.head;
+
+    if (node === null) {
+      return 0;
+    }
+
+    let count = this.head ? 1 : 0;
+    while (node.next !== null) {
+      count++;
+      node = node.next;
+    }
+
+    return count;
+  }
 }
 
 module.exports = {Node, LinkedList};
