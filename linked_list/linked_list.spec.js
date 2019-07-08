d690636f2bd5f3405a118ebb328adf0c90fe8dd1
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-08 14:37:19 -0700

    getAt tests

--- linked_list/linked_list.spec.js
+++ linked_list/linked_list.spec.js
@@ -133,4 +133,19 @@ describe('getLast', () => {
       expect(l.getLast().data).toEqual('b');
     });
   });
+
+  describe('getAt', () => {
+    test('returns the node at given index', () => {
+      const l = new List();
+      expect(l.getAt(10)).toEqual(null);
+      l.insertLast(1);
+      l.insertLast(2);
+      l.insertLast(3);
+      l.insertLast(4);
+      expect(l.getAt(0).data).toEqual(1);
+      expect(l.getAt(1).data).toEqual(2);
+      expect(l.getAt(2).data).toEqual(3);
+      expect(l.getAt(3).data).toEqual(4);
+    });
+  });
 });
