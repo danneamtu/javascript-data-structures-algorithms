2f043c538bdaaef655df7ed44e84c448f5ff196a
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-10 22:40:18 -0700

    for of tests

--- linked_list/linked_list.spec.js
+++ linked_list/linked_list.spec.js
@@ -272,4 +272,31 @@ describe('getLast', () => {
       expect(l.getAt(3).data).toEqual(14);
     });
   });
+
+  describe('for of loops', () => {
+    test('works with linked list', () => {
+      const l = new List();
+      l.insertLast(1);
+      l.insertLast(2);
+      l.insertLast(3);
+      l.insertLast(4);
+
+      for (let node of l) {
+        node.data += 10;
+      }
+
+      expect(l.getAt(0).data).toEqual(11);
+      expect(l.getAt(1).data).toEqual(12);
+      expect(l.getAt(2).data).toEqual(13);
+      expect(l.getAt(3).data).toEqual(14);
+    });
+
+    test('for of works on empty list', () => {
+      const l = new List();
+      expect(() => {
+        for (let node of l) {
+        }
+      }).not.toThrow();
+    });
+  });
 });
