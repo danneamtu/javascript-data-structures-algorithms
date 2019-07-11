62631cf46498743843e2197451afa4b4a543df2d
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-10 22:36:29 -0700

    forEach solution

--- linked_list/linked_list.js
+++ linked_list/linked_list.js
@@ -136,7 +136,14 @@ class LinkedList {
   }
 
   forEach(fn) {
-    
+    let node = this.head;
+    let counter = 0;
+
+    while (node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
   }
 }
 
