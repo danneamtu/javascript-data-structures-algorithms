0c4006f95ab255fa4a55ed34460e6766c79a4862
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-11 21:54:49 -0700

    midpont test - when a list contains 4 elements

--- midpoint/midpoint.spec.js
+++ midpoint/midpoint.spec.js
@@ -35,4 +35,13 @@ describe('midpoint returns middle node of an even numbered list', () => {
     l.insertLast('b');
     expect(midpoint(l).data).toEqual('a');
   });
+
+  test('when the list has 4 elements', () => {
+    const l = new LinkedList();
+    l.insertLast('a');
+    l.insertLast('b');
+    l.insertLast('c');
+    l.insertLast('d');
+    expect(midpoint(l).data).toEqual('b');
+  });
 });
