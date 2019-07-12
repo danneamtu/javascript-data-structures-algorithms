cc8761e4b5cd2ca6c81545a46817117ab564ffff
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-12 11:25:24 -0700

    circular test - circual at head

--- circular/circular.spec.js
+++ circular/circular.spec.js
@@ -17,4 +17,18 @@ test('circular detects circular linked lists', () => {
   b.next = c;
   c.next = b;
   expect(circular(l)).toEqual(true);
+
+  test('circular detects circular linked lists linked at the head', () => {
+    const l = new List();
+    const a = new Node('a');
+    const b = new Node('b');
+    const c = new Node('c');
+
+    l.head = a;
+    a.next = b;
+    b.next = c;
+    c.next = a;
+
+    expect(circular(l)).toEqual(true);
+  });
 });
