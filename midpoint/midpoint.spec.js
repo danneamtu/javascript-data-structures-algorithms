206bd4fa8e340cb6e1ff18ca5290dfaf0410db99
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-11 21:51:09 -0700

    midpoint test - when a list contains 3 elements

--- midpoint/midpoint.spec.js
+++ midpoint/midpoint.spec.js
@@ -8,12 +8,22 @@ test('midpoint is a function', () => {
   expect(typeof midpoint).toEqual('function');
 });
 
-test('when the list has 5 elements', () => {
-  const l = new LinkedList();
-  l.insertLast('a');
-  l.insertLast('b');
-  l.insertLast('c');
-  l.insertLast('d');
-  l.insertLast('e');
-  expect(midpoint(l).data).toEqual('c');
+describe('midpoint returns middle node of an odd numbered list', () => {
+  test('when the list has 3 elements', () => {
+    const l = new LinkedList();
+    l.insertLast('a');
+    l.insertLast('b');
+    l.insertLast('c');
+    expect(midpoint(l).data).toEqual('b');
+  });
+
+  test('when the list has 5 elements', () => {
+    const l = new LinkedList();
+    l.insertLast('a');
+    l.insertLast('b');
+    l.insertLast('c');
+    l.insertLast('d');
+    l.insertLast('e');
+    expect(midpoint(l).data).toEqual('c');
+  });
 });
