5174cca396d91960ae3d48c54b0be351b6b34c6c
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-11 22:01:20 -0700

    midpoint - first solution attempt

--- midpoint/midpoint.js
+++ midpoint/midpoint.js
@@ -1,3 +1,13 @@
-function midpoint(list) {}
+function midpoint(list) {
+  let slow = list.head;
+  let fast = list.head;
+
+  while (fast.next.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
 
 module.exports = midpoint;
