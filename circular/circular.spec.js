f2866687584c87125ccbb88f3bcf98f2c3a508ad
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-12 11:29:47 -0700

    circular test - noncircular

--- circular/circular.spec.js
+++ circular/circular.spec.js
@@ -12,23 +12,39 @@ test('circular detects circular linked lists', () => {
   const a = new Node('a');
   const b = new Node('b');
   const c = new Node('c');
+
   l.head = a;
   a.next = b;
   b.next = c;
   c.next = b;
+
   expect(circular(l)).toEqual(true);
+});
 
-  test('circular detects circular linked lists linked at the head', () => {
-    const l = new List();
-    const a = new Node('a');
-    const b = new Node('b');
-    const c = new Node('c');
+test('circular detects circular linked lists linked at the head', () => {
+  const l = new List();
+  const a = new Node('a');
+  const b = new Node('b');
+  const c = new Node('c');
 
-    l.head = a;
-    a.next = b;
-    b.next = c;
-    c.next = a;
+  l.head = a;
+  a.next = b;
+  b.next = c;
+  c.next = a;
 
-    expect(circular(l)).toEqual(true);
-  });
+  expect(circular(l)).toEqual(true);
+});
+
+test('circular detects non-circular linked lists', () => {
+  const l = new List();
+  const a = new Node('a');
+  const b = new Node('b');
+  const c = new Node('c');
+
+  l.head = a;
+  a.next = b;
+  b.next = c;
+  c.next = null;
+
+  expect(circular(l)).toEqual(false);
 });
