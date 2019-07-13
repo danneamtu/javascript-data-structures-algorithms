8f91582701c4c26632bc6bc3b68f1ed1aee406e1
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-13 05:18:22 -0700

    fromLast first solution

--- from_last/from_last.js
+++ from_last/from_last.js
@@ -1,5 +1,19 @@
 function fromLast(list, n) {
+  let slow = list.head;
+  let fast = list.head;
+  let count = 0;
 
+  while (count < n) {
+    fast = fast.next;
+    count++;
+  }
+
+  while (fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  return slow;
 }
 
-module.exports = fromLast
+module.exports = fromLast;
