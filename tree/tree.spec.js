dfd92554e7ec5d07eb1692d6a325dd8fccf3e440
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-14 09:08:49 -0700

    Node has data and children object properties

--- tree/tree.spec.js
+++ tree/tree.spec.js
@@ -6,4 +6,10 @@ describe('Node', () => {
   test('Node is a constructor', () => {
     expect(typeof Node.prototype.constructor).toEqual('function');
   });
+
+  test('Node has a data and children properties', () => {
+    const n = new Node('a');
+    expect(n.data).toEqual('a');
+    expect(n.children.length).toEqual(0);
+  });
 });
