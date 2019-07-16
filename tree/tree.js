50668c61fdf8962778b4b3fda4786b77487e12a0
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-16 10:16:00 -0700

    add children to node

--- tree/tree.js
+++ tree/tree.js
@@ -1,4 +1,14 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    const node = new Node(data);
+    this.children.push(node);
+  }
+}
 
 class Tree {}
 
