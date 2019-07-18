c5f7cea12d669d32e1f95d941ee32938607c4f4c
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-17 21:03:48 -0700

    lelvelwidth test 2

--- level_width/level_width.spec.js
+++ level_width/level_width.spec.js
@@ -5,7 +5,7 @@ test('levelWidth is a function', () => {
   expect(typeof levelWidth).toEqual('function');
 });
 
-test('levelWidth returns number of nodes at the widest point', () => {
+test('levelWidth returns the number of nodes at the widest point', () => {
   const root = new Node(0);
   root.add(1);
   root.add(2);
@@ -14,4 +14,14 @@ test('levelWidth returns number of nodes at the widest point', () => {
   root.children[2].add(5);
 
   expect(levelWidth(root)).toEqual([1, 3, 2]);
+});
+
+test('levelWidth returns the number of nodes at the widest point', () => {
+  const root = new Node(0);
+  root.add(1);
+  root.children[0].add(2);
+  root.children[0].add(3);
+  root.children[0].children[0].add(4);
+
+  expect(levelWidth(root)).toEqual([1, 1, 2, 1]);
 });
