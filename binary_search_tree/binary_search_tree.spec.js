497c70ebbf90e71ebd1127740ec31bd3215fbf1e
chrisipanaque <chris.ipanaque@gmail.com>
2019-07-19 08:07:54 -0700

    binary search tree - node can insert correctly

--- binary_search_tree/binary_search_tree.spec.js
+++ binary_search_tree/binary_search_tree.spec.js
@@ -3,3 +3,14 @@ const Node = require('./binary_search_tree');
 test('Node is a constructor', () => {
   expect(typeof Node.prototype.constructor).toEqual('function');
 });
+
+test('Node can insert correctly', () => {
+  const node = new Node(10);
+  node.insert(5);
+  node.insert(15);
+  node.insert(17);
+
+  expect(node.left.data).toEqual(5);
+  expect(node.right.data).toEqual(15);
+  expect(node.right.right.data).toEqual(17);
+});
