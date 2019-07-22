85d54b5014274230401f332f391756a63b3beed2
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-07-21 21:18:35 -0700

    events can be triggered multiple times

--- events/events.spec.js
+++ events/events.spec.js
@@ -8,7 +8,7 @@ test('Events can be registered when triggered', () => {
   events.trigger('click');
 
   expect(cb1.mock.calls.length).toBe(1);
-})
+});
 
 test('multiple events can be registered when triggered', () => {
   const events = new Events();
@@ -17,8 +17,23 @@ test('multiple events can be registered when triggered', () => {
 
   events.on('click', cb1);
   events.on('click', cb2);
-  event.trigger('click');
+  events.trigger('click');
 
   expect(cb1.mock.calls.length).toBe(1);
   expect(cb2.mock.calls.length).toBe(1);
+});
+
+test('events can be triggered multiple times', () => {
+  const events = new Events();
+  const cb1 = jest.fn();
+  const cb2 = jest.fn();
+
+  events.on('click', cb1);
+  events.trigger('click');
+  events.on('click', cb2);
+  events.trigger('click');
+  events.trigger('click');
+
+  expect(cb1.mock.calls.length).toBe(3);
+  expect(cb2.mock.calls.length).toBe(2);
 })
