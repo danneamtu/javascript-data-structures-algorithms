6db3308bd51f39ff86ed989d5878ce7cd5752324
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-07-21 20:25:07 -0700

    validate - first solution

--- validate/validate.js
+++ validate/validate.js
@@ -18,10 +18,24 @@ class Node {
   }
 }
 
-function validate(node) {
-  let max = node.data;
-  let min = null;
+function validate(node, min = null, max = null) {
+  if (max !== null && node.data < max) {
+    return false;
+  }
 
+  if (min !== null && node.data < min) {
+    return false;
+  }
+
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = { Node, validate }
