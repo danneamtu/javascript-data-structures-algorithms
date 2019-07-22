bf8de5f14bb61f6da4c892e588f4363ad5214fd5
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-07-22 10:31:40 -0700

    merge and mergeSort tests

--- sorting/sorting.spec.js
+++ sorting/sorting.spec.js
@@ -11,12 +11,24 @@ function getSortedArray() {
 describe('bubble sort', () => {
   test('sorts an array', () => {
     expect(bubbleSort(getArray())).toEqual(getSortedArray());
-  })
-})
+  });
+});
 
 describe('selection sort', () => {
   test('sorts an array', () => {
     expect(selectionSort(getArray())).toEqual(getSortedArray());
-  })
-})
+  });
+});
 
+describe('merge sort', () => {
+  test('merge function can join together two sorted arrays', () => {
+    const left = [1, 10];
+    const right = [2, 8, 12];
+
+    expect(merge(left, right)).toEqual([1, 2, 8, 10, 12]);
+  });
+
+  test('sorts an array', () => {
+    expect(mergeSort(getArray())).toEqual(getSortedArray());
+  });
+});
