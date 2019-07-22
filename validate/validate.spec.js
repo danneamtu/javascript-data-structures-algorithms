af6e835da5c80ecca70a8c4ee703cfe64d39b67f
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-07-21 18:54:42 -0700

    validate recognizes invalid binary search tree

--- validate/validate.spec.js
+++ validate/validate.spec.js
@@ -9,3 +9,14 @@ test('validate recognizes a valid Binary Search Tree', () => {
     n.insert(20)
     expect(validate(n)).toEqual(true)
 })
+
+test('validate recognizes an invalid Binary Search Tree', () => {
+    const n = new Node(10)
+    n.insert(5)
+    n.insert(15)
+    n.insert(0)
+    n.insert(20)
+    n.left.right = new Node(999)
+
+    expect(validate(n)).toEqual(false)
+})
