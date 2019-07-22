95b5632df80b9f9dc045e2180eb6b6026fc12052
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-07-22 11:45:04 -0700

    selectionSort - first solution

--- sorting/sorting.js
+++ sorting/sorting.js
@@ -11,7 +11,21 @@ function bubbleSort(arr) {
 };
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      };
+    };
+
+    if (indexOfMin !== i) {
+      [arr[indexOfMin], arr[i]] = [arr[i], arr[indexOfMin]];
+    };
+  };
+
+  return arr;
 };
 
 module.exports = { bubbleSort, selectionSort };
