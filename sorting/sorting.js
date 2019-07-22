7f97cb7efcddbc1e4aa03e9bb5f67d61912166fc
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-07-22 10:58:27 -0700

    bubble sort - second solution

--- sorting/sorting.js
+++ sorting/sorting.js
@@ -1,8 +1,8 @@
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
       };
     };
   };
