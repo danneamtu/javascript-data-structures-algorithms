b1fa52dac144b4c831ec38d6e9599d39c6627112
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-07-22 19:27:54 -0700

    merge - first solution

--- sorting/sorting.js
+++ sorting/sorting.js
@@ -28,4 +28,20 @@ function selectionSort(arr) {
   return arr;
 };
 
-module.exports = { bubbleSort, selectionSort };
+function merge(left, right) {
+  let result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  result.push(...left, ...right);
+
+  return result;
+}
+
+module.exports = { bubbleSort, selectionSort, merge };
