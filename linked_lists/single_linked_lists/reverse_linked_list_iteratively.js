378116e83eebc779e05155d94ff9b02a81ec730a
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-11-14 15:53:55 -0800

    reverse linked list recursively

--- linked_lists/single_linked_lists/reverse_linked_list_iteratively.js
+++ linked_lists/single_linked_lists/reverse_linked_list_iteratively.js
@@ -1,4 +1,13 @@
-const reverseList = head => {
+class Node {
+  constructor(value = null, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const ll = new Node(1, new Node(2, new Node(3)));
+
+const reverse_list = head => {
   if (!head) {
     return head;
   }
@@ -17,3 +26,15 @@ const reverseList = head => {
 
   return begin;
 };
+
+const reverse_list_recursively = (head, new_head = null) => {
+  if (!head) {
+    return new_head;
+  }
+
+  let node = head.next;
+  head.next = new_head;
+  return reverse_list_recursively(node, head);
+};
+
+console.log(reverse_list_recursively(ll));
