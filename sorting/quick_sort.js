eb8e12b76e4ea28e366bfe4c9f92a97e1df70525
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-11-13 20:28:34 -0800

    quick_sort fixes

--- sorting/quick_sort.js
+++ sorting/quick_sort.js
@@ -1,21 +1,28 @@
-const quick_sort_helper = arr => {
-  start = 0;
-
+const quick_sort_helper = (arr, start = 0, end = arr.length - 1) => {
   pivot_value = arr[start];
-  pivot_index = 0;
+  pivot_index = start;
 
-  for (let i = start + 1; i < arr.length; i += 1) {
+  for (let i = start + 1; i <= end; i += 1) {
     if (pivot_value > arr[i]) {
       pivot_index += 1;
       [arr[i], arr[pivot_index]] = [arr[pivot_index], arr[i]];
     }
   }
 
-  [pivot_value, arr[pivot_index]] = [arr[pivot_index], pivot_value];
-
-  console.log(arr);
+  [arr[start], arr[pivot_index]] = [arr[pivot_index], arr[start]];
 
   return pivot_index;
 };
 
-console.log(quick_sort_helper([5, 2, 7, -1, 20, 0, -5, 10]));
+const quick_sort = (arr, left = 0, right = arr.length - 1) => {
+  if (left < right) {
+    let pivot_index = quick_sort_helper(arr, left, right);
+
+    quick_sort(arr, left, pivot_index - 1);
+    quick_sort(arr, pivot_index + 1, right);
+  }
+
+  return arr;
+};
+
+console.log(quick_sort([-10, 100, 2, 7, -1, 20, 0, -5, 10]));
