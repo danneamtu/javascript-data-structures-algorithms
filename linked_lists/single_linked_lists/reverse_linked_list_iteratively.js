e0e6a7cdee1036986cdbe1934e5969646e7d7a3a
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-11-14 16:14:24 -0800

    reverse linked list recursively - time and space complexity

--- linked_lists/single_linked_lists/reverse_linked_list_iteratively.js
+++ linked_lists/single_linked_lists/reverse_linked_list_iteratively.js
@@ -7,6 +7,8 @@ class Node {
 
 const ll = new Node(1, new Node(2, new Node(3)));
 
+// time complexity - O(n), where n is the length of the linked list
+// space complexity - O(1), we are not using a data structure or a call stack
 const reverse_list = head => {
   if (!head) {
     return head;
@@ -27,14 +29,16 @@ const reverse_list = head => {
   return begin;
 };
 
+// time complexity - O(n), where n is the length of the linked list
+// space complexity - O(n), where n is the length of the linked list in the recursive call stack 
 const reverse_list_recursively = (head, new_head = null) => {
   if (!head) {
     return new_head;
   }
 
-  let node = head.next;
+  let rest = head.next;
   head.next = new_head;
-  return reverse_list_recursively(node, head);
+  return reverse_list_recursively(rest, head);
 };
 
 console.log(reverse_list_recursively(ll));
