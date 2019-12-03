e7584b108880f309166ec4518cdf8a564058241a
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-12-03 13:07:03 -0800

    consecutive Strings

---
+++ strings/consecutiveStrings.js
@@ -0,0 +1,14 @@
+/*
+You are given an array of strings called arr and an integer k. Your task is to return the longest string consisting of k consecutive strings from the array.
+
+i.e. Which "k" consecutive strings in the array are the longest?
+
+For example, if "k" is 2, and the array of strings is:
+["zone", "abigail", "theta", "form", "libe", "zas"]
+
+...then the longest two strings are "abigailtheta"
+
+longestConsecutive(["zone", "abigail", "theta", "form", "libe", "zas"], 2) // <--- "abigailtheta" 
+
+To build your algorithm, consider "n"  the length of the array of strings. If n = 0, or k > n, or k <= 0, then return "" (an empty string).
+*/
---
+++ strings/consecutiveStrings.spec.js
@@ -0,0 +1,26 @@
+const consecutiveStrings = require('./index');
+​
+describe('consecutive strings', () => {
+    describe('edge cases', () => {
+        it('returns empty string when k <= 0', () => {
+            expect(consecutiveStrings(['alpha', 'theta', 'christina'], -2)).toBe('');
+        })
+        it('returns empty string when k > arr.length', () => {
+            expect(consecutiveStrings(['beta', 'gamma', 'christina'], 6)).toBe('');
+        })
+        it('returns empty string when no length', () => {
+            expect(consecutiveStrings([])).toBe('');
+        })
+    })
+    describe('it works', () => {
+        it('returns longest string', () => {
+            expect(consecutiveStrings(["zone", "abigail", "theta", "form", "libe", "zas"], 2)).toBe('abigailtheta')
+        })
+        it('returns longest string 2', () => {
+            expect(consecutiveStrings(["apple", "banana", "kiwi", "pineapple", "strawberry", "grape"], 2)).toBe('pineapplestrawberry')
+        })
+        it('returns longest string 2', () => {
+            expect(consecutiveStrings(["Harry", "Ron", "Hermione", "Minerva", "Albus", "Draco"], 4)).toBe('HermioneMinervaAlbusDraco')
+        })
+    })
+})
