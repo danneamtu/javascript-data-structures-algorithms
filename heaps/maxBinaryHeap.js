ff3450f76d365317b3becadd8b2471ebe8eeb14e
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-12-08 15:42:43 -0800

    extract max redo

--- heaps/maxBinaryHeap.js
+++ heaps/maxBinaryHeap.js
@@ -22,50 +22,9 @@ class MaxBinaryHeap {
 
   extractMax() {
     const result = this.values[0];
+    const end = this.values.pop();
 
-    let lastIndex = this.values.length - 1;
-    let parentIndex = 0;
-
-    [this.values[parentIndex], this.values[lastIndex]] = [
-      this.values[lastIndex],
-      this.values[parentIndex],
-    ];
-
-    this.values.length--;
-
-    const childMap = {};
-
-    let left = Math.floor(parentIndex * 2 + 1);
-    let right = Math.floor(parentIndex * 2 + 2);
-
-    childMap[this.values[left]] = left;
-    childMap[this.values[right]] = right;
-
-    while (parentIndex < this.values.length) {
-      console.log('thisvalues', this.values);
-      console.log('first', childMap);
-      let max = Math.max(this.values[left], this.values[right]);
-      let maxIndex = childMap[`${max}`];
-
-      [this.values[maxIndex], this.values[parentIndex]] = [
-        this.values[parentIndex],
-        this.values[maxIndex],
-      ];
-
-      console.log('after first swap', this.values);
-
-      parentIndex = maxIndex; // 1
-      left = Math.floor(parentIndex * 2 + 1);
-      right = Math.floor(parentIndex * 2 + 2);
-
-      if (left < this.values.length) {
-        childMap[this.values[left]] = left;
-      }
-
-      if (right < this.values.length) {
-        childMap[this.values[right]] = right;
-      }
-    }
+    this.values[0] = end;
 
     return result;
   }
